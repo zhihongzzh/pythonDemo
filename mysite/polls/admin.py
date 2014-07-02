2d5b8b167cc9cf42ebf6ff89a8ebe1e58c761a06
zzh <zzh@qq.com>
2014-07-02 14:03:43 +0800

    s

---
+++ mysite/polls/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from polls.models import Poll,Choice
+
+
+# Register your models here.
+class ChoiceInline(admin.TabularInline):
+	model=Choice
+	extra=3
+class PollAdmin(admin.ModelAdmin):
+	fieldsets=[(None,{'fields':['question']}),('Date information',{'fields':['pub_date']}),]
+	inlines=[ChoiceInline]
+
+admin.site.register(Poll,PollAdmin)
+admin.site.register(Choice)
---
+++ mysite/polls/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def index(request):
+	latest_poll_list=Poll.objects.order_by('-pub_date')[:5]
+	output=',',join([p.question for p in latest_poll_list])
+	return HttpResponse(output)
+def detail(request,poll_id):
+	return HttpResponse("You're looking at poll %s." % poll_id)
+
+def results(request,poll_id):
+	return HttpResponse("You're looking at the results of poll %s." % poll_id)
+
+def vote(request,poll_id):
+	return HttpResponse("You're voting on poll %s." % poll_id)
